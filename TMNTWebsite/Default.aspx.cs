f730d02e6f34103213f44e9312f3a2b0133528d8
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2020-10-27 15:48:25 +0000

    Commented code

--- TMNTWebsite/Default.aspx.cs
+++ TMNTWebsite/Default.aspx.cs
@@ -12,41 +12,50 @@ namespace TMNTWebsite
 
         // I'm changing up the design to update automatically to be more user-friendly.
 
+        // Price vars
         float sizePrice = 0f;
         float crustPrice = 0f;
         float toppingsPrice = 0f;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            Size_Changed(null, null);
-            Crust_Changed(null, null);
-            Toppings_Changed(null, null);
+            // Do update on page load to set output to 0
+            if (!Page.IsPostBack)
+            {
+                Size_Changed(null, null);
+                Crust_Changed(null, null);
+                Toppings_Changed(null, null);
+            }
         }
 
 
         // Extra cases not needed as website has default states
         protected void Size_Changed(object sender, EventArgs e)
         {
+            // If small, £10, else if medium, £13, else £16
             sizePrice = smallRadioButton.Checked ? 10.00f : (mediumRadioButton.Checked ? 13.00f : 16.00f);
+            // Update output label
             Update_Total();
         }
 
         protected void Crust_Changed(object sender, EventArgs e)
         {
+            // Getting crust type price
             crustPrice = thinRadioButton.Checked ? 0.00f : 2.00f;
+            // Update output label
             Update_Total();
         }
 
         protected void Toppings_Changed(object sender, EventArgs e)
         {
             toppingsPrice = 0.00f;
-
+            // Ternary operator adds cost if ingredient box is checked
             toppingsPrice += pepperoniCheckBox.Checked ? 1.50f : 0f;
             toppingsPrice += onionsCheckBox.Checked ? 0.75f : 0f;
             toppingsPrice += greenPeppersCheckBox.Checked ? 0.50f : 0f;
             toppingsPrice += redPeppersCheckBox.Checked ? 0.75f : 0f;
             toppingsPrice += anchoviesCheckBox.Checked ? 2.00f : 0f;
-
+            // Update output label
             Update_Total();
         }
 
@@ -54,14 +63,14 @@ namespace TMNTWebsite
         {
             float total = sizePrice + crustPrice + toppingsPrice;
 
-            // Special offer. This is not a good solution at all but it is all the task requires.
+            // Checking for special offer. This is not a good solution at all but it is all the task requires. Ideally would use OOP to have an "Offer" object containing the requirements.
             if(pepperoniCheckBox.Checked && greenPeppersCheckBox.Checked && anchoviesCheckBox.Checked ||
                 pepperoniCheckBox.Checked && redPeppersCheckBox.Checked && onionsCheckBox.Checked)
             {
                 total -= 2.00f;
             }
 
-
+            // Updating the output label
             totalLabel.Text = "£" + total.ToString();
         }
 
